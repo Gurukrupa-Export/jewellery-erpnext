8d363c9ef5c73cd9b63e70c0171b3274fe0beb9a
Dharm Vaghasiya <dharm_v@gkexport.com>
2025-03-08 12:47:35 +0530

    Update in Sketch Order Form

--- jewellery_erpnext/gurukrupa_exports/doctype/sketch_order/sketch_order.py
+++ jewellery_erpnext/gurukrupa_exports/doctype/sketch_order/sketch_order.py
@@ -17,19 +17,24 @@ class SketchOrder(Document):
 
 	def make_items(self):
 		# if self.workflow_state == "Items Updated":
-		for row in self.final_sketch_approval_cmo:
-			# if row.item or not (row.design_status == "Approved" and row.design_status_cpo == "Approved"):
-			# 	continue
-			item_template = create_item_template_from_sketch_order(self, row.name)
+		if self.order_type != 'Purchase':
+			for row in self.final_sketch_approval_cmo:
+				# if row.item or not (row.design_status == "Approved" and row.design_status_cpo == "Approved"):
+				# 	continue
+				item_template = create_item_template_from_sketch_order(self, row.name)
+				updatet_item_template(self, item_template)
+				# item_variant = create_item_from_sketch_order(self, item_template, row.name)
+				# update_item_variant(self, item_variant, item_template)
+				frappe.db.set_value(row.doctype, row.name, "item", item_template)
+				frappe.msgprint(_("New Item Created: {0}").format(get_link_to_form("Item", item_template)))
+		if self.order_type == 'Purchase':
+			item_template = create_item_for_po(self,self.name)
 			updatet_item_template(self, item_template)
-			# item_variant = create_item_from_sketch_order(self, item_template, row.name)
-			# update_item_variant(self, item_variant, item_template)
-			frappe.db.set_value(row.doctype, row.name, "item", item_template)
+			frappe.db.set_value("Sketch Order", self.name, "item_code", item_template)
 			frappe.msgprint(_("New Item Created: {0}").format(get_link_to_form("Item", item_template)))
 
 def updatet_item_template(self, item_template):
 	frappe.db.set_value("Item", item_template, {"is_design_code": 0, "item_code": item_template})
-
 
 def update_item_variant(self, item_variant, item_template):
 	frappe.db.set_value("Item", item_variant, {"is_design_code": 1, "variant_of": item_template})
@@ -123,7 +128,6 @@ def populate_child_table(self):
 				self.append("final_sketch_approval_cmo", {"designer": j["designer"]})
 			designer.append(j["designer"])
 
-
 def check_count(self, designer):
 	count = 0
 	if self.final_sketch_approval_cmo:
@@ -132,7 +136,6 @@ def check_count(self, designer):
 				count += 1
 
 	return count
-
 
 def create_system_notification(self, subject, context, recipients):
 	if not recipients:
@@ -156,7 +159,6 @@ def create_system_notification(self, subject, context, recipients):
 			or notification_doc.get("type") == "Alert"
 		):
 			notification.insert(ignore_permissions=True)
-
 
 @frappe.whitelist()
 def create_item_template_from_sketch_order(self, source_name, target_doc=None):
@@ -309,117 +311,158 @@ def create_item_from_sketch_order(self, item_template, source_name, target_doc=N
 	return doc.name
 
 
-def create_only_variant_from_order(self, source_name, target_doc=None):
-	db_data = frappe.db.get_list(
-		"Item", filters={"name": self.tag__design_id}, fields=["variant_of"], order_by="creation desc"
-	)[0]
-	db_data1 = frappe.db.get_list(
-		"Item", filters={"variant_of": db_data["variant_of"]}, fields=["name"], order_by="creation desc"
-	)[0]
+# def create_only_variant_from_order(self, source_name, target_doc=None):
+# 	db_data = frappe.db.get_list(
+# 		"Item", filters={"name": self.tag__design_id}, fields=["variant_of"], order_by="creation desc"
+# 	)[0]
+# 	db_data1 = frappe.db.get_list(
+# 		"Item", filters={"variant_of": db_data["variant_of"]}, fields=["name"], order_by="creation desc"
+# 	)[0]
 
+# 	def post_process(source, target):
+
+# 		index = int(db_data1["name"].split("-")[1]) + 1
+# 		suffix = "%.3i" % index
+# 		item_code = db_data["variant_of"] + "-" + suffix
+# 		target.item_code = item_code
+# 		target.sequence = item_code[2:7]
+
+# 		target.india = self.india
+# 		target.india_states = self.india_states
+# 		target.usa = self.usa
+# 		target.usa_states = self.usa_states
+# 		target.custom_sketch_order_id = self.name
+# 		target.custom_sketch_order_form_id = self.sketch_order_form
+# 		sub_category = frappe.db.get_value("Final Sketch Approval CMO", source_name, "sub_category")
+# 		designer = frappe.db.get_value("Final Sketch Approval CMO", source_name, "designer")
+# 		target.item_group = sub_category + " - V"
+# 		target.designer = designer
+# 		target.order_form_type = "Sketch Order"
+
+# 		for row in self.age_group:
+# 			target.append("custom_age_group", {"design_attribute": row.design_attribute})
+
+# 		for row in self.alphabetnumber:
+# 			target.append("custom_alphabetnumber", {"design_attribute": row.design_attribute})
+
+# 		for row in self.animalbirds:
+# 			target.append("custom_animalbirds", {"design_attribute": row.design_attribute})
+
+# 		for row in self.collection_1:
+# 			target.append("custom_collection", {"design_attribute": row.design_attribute})
+
+# 		for row in self.design_style:
+# 			target.append("custom_design_style", {"design_attribute": row.design_attribute})
+
+# 		for row in self.gender:
+# 			target.append("custom_gender", {"design_attribute": row.design_attribute})
+
+# 		for row in self.lines_rows:
+# 			target.append("custom_lines__rows", {"design_attribute": row.design_attribute})
+
+# 		for row in self.language:
+# 			target.append("custom_language", {"design_attribute": row.design_attribute})
+
+# 		for row in self.occasion:
+# 			target.append("custom_occasion", {"design_attribute": row.design_attribute})
+
+# 		for row in self.religious:
+# 			target.append("custom_religious", {"design_attribute": row.design_attribute})
+
+# 		for row in self.shapes:
+# 			target.append("custom_shapes", {"design_attribute": row.design_attribute})
+
+# 		for row in self.zodiac:
+# 			target.append("custom_zodiac", {"design_attribute": row.design_attribute})
+
+# 		for row in self.rhodium:
+# 			target.append("custom_rhodium", {"design_attribute": row.design_attribute})
+
+# 		# attribute_value_for_name = []
+# 		# for i in frappe.get_all(
+# 		# 	"Attribute Value Item Attribute Detail",
+# 		# 	{
+# 		# 		"parent": sub_category,
+# 		# 		"in_item_variant": 1,
+# 		# 	},
+# 		# 	"item_attribute",
+# 		# 	order_by="idx asc",
+# 		# ):
+# 		# 	attribute_with = i.item_attribute.lower().replace(" ", "_")
+# 		# 	attribute_value = None
+
+# 		# 	field_data = frappe.get_meta("Sketch Order").get_field(attribute_with)
+# 		# 	if field_data and field_data.fieldtype != "Table MultiSelect":
+# 		# 		attribute_value = frappe.db.get_value("Sketch Order", self.name, attribute_with)
+
+# 		# 	target.append(
+# 		# 		"attributes",
+# 		# 		{
+# 		# 			"attribute": i.item_attribute,
+# 		# 			"variant_of": db_data,
+# 		# 			"attribute_value": attribute_value,
+# 		# 		},
+# 		# 	)
+
+# 	doc = get_mapped_doc(
+# 		"Final Sketch Approval CMO",
+# 		source_name,
+# 		{
+# 			"Final Sketch Approval CMO": {
+# 				"doctype": "Item",
+# 				"field_map": {
+# 					"category": "item_category",
+# 					"sub_category": "item_subcategory",
+# 					"gold_wt_approx": "approx_gold",
+# 					"diamond_wt_approx": "approx_diamond",
+# 					"designer": "designer",
+# 				},
+# 			}
+# 		},
+# 		target_doc,
+# 		post_process,
+# 	)
+# 	# frappe.throw(f"{doc}")
+# 	doc.save()
+# 	return doc.name, db_data["variant_of"]
+
+def create_item_for_po(self, source_name, target_doc=None):
 	def post_process(source, target):
 
-		index = int(db_data1["name"].split("-")[1]) + 1
-		suffix = "%.3i" % index
-		item_code = db_data["variant_of"] + "-" + suffix
-		target.item_code = item_code
-		target.sequence = item_code[2:7]
-
+		target.is_design_code = 1
+		target.has_variants = 1
 		target.india = self.india
 		target.india_states = self.india_states
 		target.usa = self.usa
 		target.usa_states = self.usa_states
+		if frappe.db.get_value('Employee',{'user_id':frappe.session.user},'name'):
+			target.designer = frappe.db.get_value('Employee',{'user_id':frappe.session.user},'name')
+		else:
+			target.designer = frappe.db.get_value('User',frappe.session.user,'full_name')
+		# new code start
 		target.custom_sketch_order_id = self.name
 		target.custom_sketch_order_form_id = self.sketch_order_form
-		sub_category = frappe.db.get_value("Final Sketch Approval CMO", source_name, "sub_category")
-		designer = frappe.db.get_value("Final Sketch Approval CMO", source_name, "designer")
-		target.item_group = sub_category + " - V"
-		target.designer = designer
-		target.order_form_type = "Sketch Order"
-
-		for row in self.age_group:
-			target.append("custom_age_group", {"design_attribute": row.design_attribute})
-
-		for row in self.alphabetnumber:
-			target.append("custom_alphabetnumber", {"design_attribute": row.design_attribute})
-
-		for row in self.animalbirds:
-			target.append("custom_animalbirds", {"design_attribute": row.design_attribute})
-
-		for row in self.collection_1:
-			target.append("custom_collection", {"design_attribute": row.design_attribute})
-
-		for row in self.design_style:
-			target.append("custom_design_style", {"design_attribute": row.design_attribute})
-
-		for row in self.gender:
-			target.append("custom_gender", {"design_attribute": row.design_attribute})
-
-		for row in self.lines_rows:
-			target.append("custom_lines__rows", {"design_attribute": row.design_attribute})
-
-		for row in self.language:
-			target.append("custom_language", {"design_attribute": row.design_attribute})
-
-		for row in self.occasion:
-			target.append("custom_occasion", {"design_attribute": row.design_attribute})
-
-		for row in self.religious:
-			target.append("custom_religious", {"design_attribute": row.design_attribute})
-
-		for row in self.shapes:
-			target.append("custom_shapes", {"design_attribute": row.design_attribute})
-
-		for row in self.zodiac:
-			target.append("custom_zodiac", {"design_attribute": row.design_attribute})
-
-		for row in self.rhodium:
-			target.append("custom_rhodium", {"design_attribute": row.design_attribute})
-
-		# attribute_value_for_name = []
-		# for i in frappe.get_all(
-		# 	"Attribute Value Item Attribute Detail",
-		# 	{
-		# 		"parent": sub_category,
-		# 		"in_item_variant": 1,
-		# 	},
-		# 	"item_attribute",
-		# 	order_by="idx asc",
-		# ):
-		# 	attribute_with = i.item_attribute.lower().replace(" ", "_")
-		# 	attribute_value = None
-
-		# 	field_data = frappe.get_meta("Sketch Order").get_field(attribute_with)
-		# 	if field_data and field_data.fieldtype != "Table MultiSelect":
-		# 		attribute_value = frappe.db.get_value("Sketch Order", self.name, attribute_with)
-
-		# 	target.append(
-		# 		"attributes",
-		# 		{
-		# 			"attribute": i.item_attribute,
-		# 			"variant_of": db_data,
-		# 			"attribute_value": attribute_value,
-		# 		},
-		# 	)
+		target.item_group = self.subcategory + " - T"
+		target.item_category = self.category
+		target.item_subcategory = self.subcategory
+		# new code end
 
 	doc = get_mapped_doc(
-		"Final Sketch Approval CMO",
-		source_name,
+		"Sketch Order",
+		self.name,
 		{
-			"Final Sketch Approval CMO": {
+			"Sketch Order": {
 				"doctype": "Item",
 				"field_map": {
 					"category": "item_category",
-					"sub_category": "item_subcategory",
-					"gold_wt_approx": "approx_gold",
-					"diamond_wt_approx": "approx_diamond",
-					"designer": "designer",
+					# "metal_target": "approx_gold",
+					# "diamond_target": "approx_diamond",
 				},
 			}
 		},
 		target_doc,
 		post_process,
 	)
-	# frappe.throw(f"{doc}")
 	doc.save()
-	return doc.name, db_data["variant_of"]
+	return doc.name
+
