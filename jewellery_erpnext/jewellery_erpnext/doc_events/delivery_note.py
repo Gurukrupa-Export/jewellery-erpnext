adb4a130d88f29f545d8e54b706833795348a1cd
Dharm Vaghasiya <dharm_v@gkexport.com>
2025-05-12 16:44:58 +0530

    Update Prachi's Sales Point
    Update Delivery Note py file

--- jewellery_erpnext/jewellery_erpnext/doc_events/delivery_note.py
+++ jewellery_erpnext/jewellery_erpnext/doc_events/delivery_note.py
@@ -4,7 +4,7 @@ from frappe import _
 def validate(self, method):
     self.set("custom_invoice_item", [])
     added_items = set()  
-    
+
     for row in self.items:
         if row.against_sales_order:
             sales_order_id = row.against_sales_order
@@ -12,7 +12,7 @@ def validate(self, method):
             invoice_items = frappe.get_all(
                 'Sales Order E Invoice Item',
                 filters={'parent': sales_order_id},
-                fields=['item_code', 'item_name', 'uom', 'qty', 'rate', 'amount']
+                fields=['item_code', 'item_name', 'uom', 'qty', 'rate', 'amount',"tax_amount","amount_with_tax","tax_rate"]
             )
             
             for invoice_item in invoice_items:
@@ -33,5 +33,17 @@ def validate(self, method):
                         'uom': invoice_item.uom,
                         'qty': invoice_item.qty,
                         'rate': invoice_item.rate,
-                        'amount': invoice_item.amount
+                        'amount': invoice_item.amount,
+                        "tax_amount":invoice_item.tax_amount,
+                        "amount_with_tax":invoice_item.amount_with_tax,
+                        "tax_rate":invoice_item.tax_rate
                     })
+
+    for r in self.items:
+        if r.serial_no:
+            source_warehouse=frappe.db.get_value('Serial No',r.serial_no,'warehouse')
+            # frappe.throw(f"{row}")
+            self.set_warehouse=source_warehouse
+            r.warehouse=source_warehouse
+
+    
