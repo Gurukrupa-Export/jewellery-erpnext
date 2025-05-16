ca5eda300e60834b604529e8d2fc28e19c2fd607
Dharm Vaghasiya <160316964+dharm-gkexport@users.noreply.github.com>
2025-05-16 17:47:15 +0530

    Update manufacturing_work_order.py
    Company to Manufactre

--- jewellery_erpnext/jewellery_erpnext/doctype/manufacturing_work_order/manufacturing_work_order.py
+++ jewellery_erpnext/jewellery_erpnext/doctype/manufacturing_work_order/manufacturing_work_order.py
@@ -44,8 +44,11 @@ class ManufacturingWorkOrder(Document):
 		self.db_set("status", "Not Started")
 
 	def validate_other_work_orders(self):
+		# last_department = frappe.db.get_value(
+		# 	"Department Operation", {"is_last_operation": 1, "company": self.company}, "department"
+		# )
 		last_department = frappe.db.get_value(
-			"Department Operation", {"is_last_operation": 1, "company": self.company}, "department"
+			"Department Operation", {"is_last_operation": 1, "manufacturer": self.manufacturer}, "department"
 		)
 		if not last_department:
 			frappe.throw(_("Please set last operation first in Department Operation"))
