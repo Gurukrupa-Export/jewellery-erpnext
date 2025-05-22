c8d82d1e432b7dbaf67613d8aec8a08e703e4b10
Dharm Vaghasiya <dharm_v@gkexport.com>
2025-05-22 13:53:17 +0530

    Update Batch Number Logic
    Add UUID instead of Fix number in Batch

--- jewellery_erpnext/jewellery_erpnext/customization/batch/batch.py
+++ jewellery_erpnext/jewellery_erpnext/customization/batch/batch.py
@@ -4,24 +4,24 @@ from jewellery_erpnext.jewellery_erpnext.customization.batch.doc_events.utils im
 )
 import frappe
 import datetime
+import random
+import string
 
 def validate(self, method):
 	update_pure_qty(self)
 	update_inventory_dimentions(self)
 
 def autoname(self,method=None):
-	# frappe.throw(f"{self.name}")
-	year_code = get_year_code()
-	month_code = get_month_code()
-	week_code = get_week_code()
+	# year_code = get_year_code()
+	# month_code = get_month_code()
+	# week_code = get_week_code()
 	item_group = frappe.db.get_value("Item",{self.item},"item_group")
 
-	# frappe.throw(f"{item_group}")
 	if item_group in ["Metal - V", "Diamond - V", "Gemstone - V", "Finding - V", "Other - V"]:
 		year_code = get_year_code()
 		month_code = get_month_code()
 		week_code = get_week_code()
-		start_of_week, end_of_week = get_current_week_date_range()
+		# start_of_week, end_of_week = get_current_week_date_range()
 
 		if item_group == "Diamond - V":
 			batch_number = "GE{year_code}{month_code}{week_code}-D".format(
@@ -57,23 +57,24 @@ def autoname(self,method=None):
 				else:
 					frappe.throw(("Abbrivation is missing for {0}").format(i.attribute_value))
 		batch_code = batch_number + "".join(batch_abbr_code_list)
-		batch_list = frappe.db.sql(f"""SELECT 
-										name
-									FROM 
-										`tabBatch`
-									WHERE 
-										manufacturing_date > '{start_of_week}'
-										AND manufacturing_date < '{end_of_week}' 
-										AND item = '{self.item}'
-									ORDER BY 
-										CAST(SUBSTRING_INDEX(name, '-', -1) AS UNSIGNED) DESC;
-									""",as_dict=1)
-		if batch_list:
-			batch = batch_list[0]["name"].split('-')[-1]
-			sequence = int(batch) + 1
-			sequence = f"{sequence:04}"
-		else:
-			sequence = '0001'
+		# batch_list = frappe.db.sql(f"""SELECT 
+		# 								name
+		# 							FROM 
+		# 								`tabBatch`
+		# 							WHERE 
+		# 								manufacturing_date > '{start_of_week}'
+		# 								AND manufacturing_date < '{end_of_week}' 
+		# 								AND item = '{self.item}'
+		# 							ORDER BY 
+		# 								CAST(SUBSTRING_INDEX(name, '-', -1) AS UNSIGNED) DESC;
+		# 							""",as_dict=1)
+		# if batch_list:
+		# 	batch = batch_list[0]["name"].split('-')[-1]
+		# 	sequence = int(batch) + 1
+		# 	sequence = f"{sequence:04}"
+		# else:
+		# 	sequence = '0001'
+		sequence = generate_unique_alphanumeric()
 		self.name = batch_code + '-' + sequence
 
 
@@ -94,39 +95,52 @@ def get_year_code():
 	last_two_digits = current_year % 100
 	return str(last_two_digits)[0] + year_dict[str(last_two_digits)[1]]
 
-
 def get_week_code():
 	current_date = datetime.date.today()
 	week_number = (current_date.day - 1) // 7 + 1
 	return str(week_number)
-
 
 def get_month_code():
 	current_date = datetime.datetime.now()
 	month_two_digit = current_date.strftime("%m")
 	return str(month_two_digit)
 
-def get_current_week_date_range():
-	current_date = datetime.date.today()
-	first_day_of_month = current_date.replace(day=1)
+# def get_current_week_date_range():
+# 	current_date = datetime.date.today()
+# 	first_day_of_month = current_date.replace(day=1)
 
-	# Calculate start of the week
-	day_of_week = current_date.weekday()  # Monday is 0, Sunday is 6
-	start_of_week = current_date - datetime.timedelta(days=day_of_week)
+# 	# Calculate start of the week
+# 	day_of_week = current_date.weekday()  # Monday is 0, Sunday is 6
+# 	start_of_week = current_date - datetime.timedelta(days=day_of_week)
 
-	# Make sure the week doesn't start before the first of the month
-	start_of_week = max(start_of_week, first_day_of_month)
+# 	# Make sure the week doesn't start before the first of the month
+# 	start_of_week = max(start_of_week, first_day_of_month)
 
-	# Calculate end of the week
-	end_of_week = start_of_week + datetime.timedelta(days=6)
+# 	# Calculate end of the week
+# 	end_of_week = start_of_week + datetime.timedelta(days=6)
 
-	# Make sure the week doesn't extend beyond the month
-	last_day_of_month = (
-		current_date.replace(day=28) + datetime.timedelta(days=4)
-	).replace(day=1) - datetime.timedelta(days=1)
-	end_of_week = min(end_of_week, last_day_of_month)
+# 	# Make sure the week doesn't extend beyond the month
+# 	last_day_of_month = (
+# 		current_date.replace(day=28) + datetime.timedelta(days=4)
+# 	).replace(day=1) - datetime.timedelta(days=1)
+# 	end_of_week = min(end_of_week, last_day_of_month)
 
-	start_formatted = start_of_week.strftime("%Y-%-m-%-d")
-	end_formatted = end_of_week.strftime("%Y-%-m-%-d")
+# 	start_formatted = start_of_week.strftime("%Y-%-m-%-d")
+# 	end_formatted = end_of_week.strftime("%Y-%-m-%-d")
 
-	return start_formatted, end_formatted
+# 	return start_formatted, end_formatted
+
+def generate_unique_alphanumeric():
+    while True:
+        # Ensure at least one letter and one number
+        letters = random.choices(string.ascii_uppercase, k=2)  # At least 2 letters
+        digits = random.choices(string.digits, k=3)  # At least 3 numbers
+        random_code = ''.join(random.sample(letters + digits, 5))  # Shuffle & combine
+
+        # Check if it already exists
+        existing_doc = frappe.get_value("Manufacturing Operation", {"name": f"MOP-{random_code}"}, "name")
+
+        if not existing_doc:  # If unique, return it
+            return random_code
+		
+
