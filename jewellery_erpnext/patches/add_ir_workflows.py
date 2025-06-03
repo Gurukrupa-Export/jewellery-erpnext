eeb4e50ad4ef730c1a35335983fef31a43931e43
Kavin <78342682+kavin0411@users.noreply.github.com>
2025-06-03 16:18:53 +0530

    fix: add IR Issued and IR Received

--- jewellery_erpnext/patches/add_ir_workflows.py
+++ jewellery_erpnext/patches/add_ir_workflows.py
@@ -20,11 +20,11 @@ def create_ir_workflow(doctype):
 				make_state("MOP Failed", 0, "Danger"),
 				make_state("Pending Stock Entry Creation", 0, "Warning"),
 				make_state("Queued Stock Entry Creation", 0, "Primary"),
-				make_state("Stock Entry Created", 1, "Success"),
+				make_state("Stock Entry Created", 0, "Success"),
 				make_state("Stock Entry Failed", 0, "Danger"),
-				make_state("Cancelled", 2, "Dark"),
-				make_state("IR Issued", 1, "Success"),
-				make_state("IR Received", 1, "Success"),
+				make_state("Cancelled", 2, "Danger"),
+				make_state("IR Issued", 1, "Success", "doc.type == 'Issue'"),
+				make_state("IR Received", 1, "Success", "doc.type == 'Receive'"),
 			],
 			"transitions": [
 				make_transition("Pending MOP Creation", "Create MOP", "Queued MOP Creation"),
@@ -32,7 +32,8 @@ def create_ir_workflow(doctype):
 				make_transition("Pending Stock Entry Creation", "Create Stock Entry", "Queued Stock Entry Creation"),
 				make_transition("Queued Stock Entry Creation", "Cancel Stock Entry Queue", "Stock Entry Failed"),
 				make_transition("Stock Entry Failed", "Retry Stock Entry Creation", "Queued Stock Entry Creation"),
-				make_transition("Stock Entry Created", "Cancel", "Cancelled"),
+				make_transition("IR Issued", "Cancel", "Cancelled", "doc.type == 'Issue'"),
+				make_transition("IR Received", "Cancel", "Cancelled", "doc.type == 'Receive'"),
 			],
 		}
 	)
@@ -40,13 +41,14 @@ def create_ir_workflow(doctype):
 	frappe.msgprint(f"{doctype} Workflow created.")
 
 
-def make_state(name, doc_status=0, style=""):
+def make_state(name, doc_status=0, style="", condition=None):
 	if not frappe.db.exists("Workflow State", name):
 			workflow_state = frappe.get_doc(
 				{
 					"doctype": "Workflow State",
 					"workflow_state_name": name,
 					"style": style,
+					"condition": condition,
 				}
 			)
 			workflow_state.insert()
@@ -56,10 +58,11 @@ def make_state(name, doc_status=0, style=""):
 		"doc_status": doc_status,
 		"allow_edit": "All",
 		"style": style,
+		"condition": condition
 	}
 
 
-def make_transition(state, action, next_state):
+def make_transition(state, action, next_state, condition):
 	if not frappe.db.exists("Workflow Action Master", action):
 			workflow_state = frappe.get_doc(
 				{
@@ -75,4 +78,5 @@ def make_transition(state, action, next_state):
 		"next_state": next_state,
 		"allowed": "All",
 		"allow_self_approval": 1,
+		"condition": condition
 	}
