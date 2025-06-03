d3ace631570e0f6f81e0e247a9081eb0658a4397
Kavin <78342682+kavin0411@users.noreply.github.com>
2025-06-03 19:51:16 +0530

    fix

--- jewellery_erpnext/patches/update_employee_ir_status.py
+++ jewellery_erpnext/patches/update_employee_ir_status.py
@@ -1,7 +1,8 @@
 import frappe
 
-employee_ir_list = frappe.db.get_list("Employee IR", {"docstatus":1, "type": "Receive"}, pluck="name")
-for ir in employee_ir_list:
-    frappe.db.set_value("Employee IR", ir, "workflow_state", "IR Received", update_modified=False)
+def execute():
+    employee_ir_list = frappe.db.get_all("Employee IR", {"docstatus":1, "type": "Receive"}, pluck="name")
+    for ir in employee_ir_list:
+        frappe.db.set_value("Employee IR", ir, "workflow_state", "IR Received", update_modified=False)
 
-frappe.db.commit()
+    frappe.db.commit()
