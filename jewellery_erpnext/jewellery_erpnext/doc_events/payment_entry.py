94c5c7c604707a833a1cde936553c858dae0ae33
Kavin <78342682+kavin0411@users.noreply.github.com>
2025-06-04 17:28:39 +0530

    refactor: create_journal_entry for branch accounts

--- jewellery_erpnext/jewellery_erpnext/doc_events/payment_entry.py
+++ jewellery_erpnext/jewellery_erpnext/doc_events/payment_entry.py
@@ -8,8 +8,7 @@ def on_submit(doc, method):
 		return
 
 	pe_branch = doc.branch
-	paid_from_account = doc.paid_from
-	paid_to_account = doc.paid_to
+	receivable_account = doc.paid_from
 
 	if not pe_branch:
 		return
@@ -24,10 +23,10 @@ def on_submit(doc, method):
 		si_branch = is_different_branch_ad(si_name, pe_branch)
 
 		if si_branch:
+
 			jv = create_journal_entry_for_different_branch(
 				doc,
-				paid_from_account,
-				paid_to_account,
+				receivable_account,
 				si_branch,
 				pe_branch,
 				row.allocated_amount
@@ -56,49 +55,64 @@ def on_cancel(doc, method):
 
 def is_different_branch_ad(si_name, pe_branch):
 	si_branch = frappe.db.get_value("Sales Invoice", si_name, "branch")
-	if si_branch and si_branch != pe_branch:
+	if si_branch and (si_branch != pe_branch):
 		return si_branch
 
 	return False
 
 
-def create_journal_entry_for_different_branch(doc, paid_from_account, paid_to_account, si_branch, pe_branch, amount):
+def create_journal_entry_for_different_branch(doc, receivable_account, si_branch, pe_branch, amount):
 	"""
 	Create a journal entry for the different branch.
 	"""
+	# get branch accounts if not raise error
+	si_branch_account = get_branch_account(si_branch)
+	pe_branch_account = get_branch_account(pe_branch)
+
 	jv = frappe.new_doc("Journal Entry")
 	jv.voucher_type = "Journal Entry"
 	jv.company = doc.company
 	jv.posting_date = doc.posting_date
 
+
 	jv.set("accounts", [
 		{
-			"account": paid_to_account,
-			"debit_in_account_currency": amount,
-			"branch": si_branch,
-		},
-		{
-			"account": paid_to_account,
-			"credit_in_account_currency": amount,
-			"branch": pe_branch
-		},
-		{
-			"account": paid_from_account,
-			"debit_in_account_currency": amount,
-			"party_type": doc.party_type,
-			"party": doc.party,
-			"branch": pe_branch,
-		},
-		{
-			"account": paid_from_account,
+			"account": receivable_account,
 			"credit_in_account_currency": amount,
 			"party_type": doc.party_type,
 			"party": doc.party,
-			"branch": si_branch
+		},
+		{
+			"account": si_branch_account,
+			"debit_in_account_currency": amount,
+		},
+		{
+			"account": pe_branch_account,
+			"credit_in_account_currency": amount,
+		},
+		{
+			"account": receivable_account,
+			"debit_in_account_currency": amount,
+			"party_type": doc.party_type,
+			"party": doc.party,
 		}
+
 	])
 
 	jv.insert()
 	jv.submit()
 
 	return jv.name
+
+
+def get_branch_account(branch_name):
+	"""
+	Get the branch account for the given branch name.
+	"""
+	if branch_account:= frappe.db.get_value("Branch", branch_name, "branch_account"):
+		return branch_account
+
+	if branch_account:= frappe.db.exists("Account", {"account_name":branch_name}):
+		return branch_account
+
+	frappe.throw(f"Branch account for {branch_name} does not exist. Please create it first.")
