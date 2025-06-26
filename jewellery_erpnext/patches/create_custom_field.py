50a12014895950a2cf25abb1f6ce49177eb44788
Dharm Vaghasiya <160316964+dharm-gkexport@users.noreply.github.com>
2025-06-26 18:45:18 +0530

    Update create_custom_field.py

--- jewellery_erpnext/patches/create_custom_field.py
+++ jewellery_erpnext/patches/create_custom_field.py
@@ -8,6 +8,6 @@ def execute():
 			with open(os.path.join(path, file), "r") as f:
 				CUSTOM_FIELDS.update(json.load(f))
 
-	from frappe.custom.doctype.custom_field.custom_field import create_custom_fields
+	# from frappe.custom.doctype.custom_field.custom_field import create_custom_fields
 
-	create_custom_fields(CUSTOM_FIELDS)
+	# create_custom_fields(CUSTOM_FIELDS)
