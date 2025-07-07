a0e9dff7831c083763068e21c38a27599b6a2de9
Kavin <78342682+kavin0411@users.noreply.github.com>
2025-07-07 16:58:39 +0530

    feat: add cron job for stock reconc from template

--- jewellery_erpnext/jewellery_erpnext/doctype/stock_reconciliation_template/test_stock_reconciliation_template.py
+++ jewellery_erpnext/jewellery_erpnext/doctype/stock_reconciliation_template/test_stock_reconciliation_template.py
@@ -5,5 +5,5 @@
 from frappe.tests.utils import FrappeTestCase
 
 
-class TestStockReconciliationtemplate(FrappeTestCase):
+class TestStockReconciliationTemplate(FrappeTestCase):
 	pass
--- jewellery_erpnext/jewellery_erpnext/doctype/stock_reconciliation_template_item/stock_reconciliation_template_item.py
+++ jewellery_erpnext/jewellery_erpnext/doctype/stock_reconciliation_template_item/stock_reconciliation_template_item.py
@@ -5,5 +5,5 @@
 from frappe.model.document import Document
 
 
-class StockReconciliationtemplateItem(Document):
+class StockReconciliationTemplateItem(Document):
 	pass
