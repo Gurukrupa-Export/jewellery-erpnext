4d1897cd2e98c68a4bdac866f9b408ce8d7e128a
Dhinesh <dhinesh@aerele.in>
2025-08-12 18:37:38 +0530

    fix: Subtract the difference in losses equally from the items present on the map.

--- jewellery_erpnext/jewellery_erpnext/doctype/employee_ir/doc_events/employee_ir_utils.py
+++ jewellery_erpnext/jewellery_erpnext/doctype/employee_ir/doc_events/employee_ir_utils.py
@@ -349,6 +349,88 @@ def create_chain_stock_entry(self, row):
 				)
 
 		if temp_diff > 0:
+			mop_data = frappe.db.get_all(
+				"MOP Balance Table",
+				{"parent": row.manufacturing_operation},
+				["item_code", "batch_no", "qty", "parent", "inventory_type", "s_warehouse"],
+			)
+			manual_loss_items = [
+				{
+					"item_code": loss_item.item_code,
+					"loss_qty": loss_item.proportionally_loss,
+					"batch_no": loss_item.batch_no,
+					"inventory_type": loss_item.inventory_type,
+					"customer": loss_item.customer,
+					"pcs": loss_item.pcs,
+					"manufacturing_work_order": loss_item.manufacturing_work_order,
+					"manufacturing_operation": loss_item.manufacturing_operation,
+					"variant_of": loss_item.variant_of,
+					"sub_setting_type": loss_item.sub_setting_type,
+					"loss_type": loss_item.loss_type,
+				}
+				for loss_item in self.manually_book_loss_details
+				if loss_item.manufacturing_work_order == row.manufacturing_work_order
+			]
+			proprtionate_loss_items = [
+				{
+					"item_code": loss_item.item_code,
+					"loss_qty": loss_item.proportionally_loss,
+					"batch_no": loss_item.batch_no,
+					"inventory_type": loss_item.inventory_type,
+					"customer": loss_item.customer,
+					"pcs": loss_item.pcs,
+					"manufacturing_work_order": loss_item.manufacturing_work_order,
+					"manufacturing_operation": loss_item.manufacturing_operation,
+					"loss_type": loss_item.loss_type,
+				}
+				for loss_item in self.employee_loss_details
+				if loss_item.manufacturing_work_order == row.manufacturing_work_order
+			]
+			if manual_loss_items or proprtionate_loss_items:
+				process_loss_entry(
+					self,
+					row,
+					manual_loss_items,
+					proprtionate_loss_items,
+					emp_wh,
+					mfg_warehouse,
+					department_wh,
+					mop_data,
+				)
+			temp_diff = 0
+			mop_batch_list = []
+			if self.main_slip:
+				temp_diff, mop_batch_list = create_repack(
+					self, row, item, mop_data, warehouse, row.received_gross_wt
+				)
+			for batch in mop_batch_list:
+				se_doc.append(
+					"items",
+					{
+						"item_code": item,
+						"s_warehouse": warehouse,
+						"t_warehouse": mfg_warehouse,
+						"to_employee": None,
+						"employee": self.employee,
+						"to_subcontractor": None,
+						"use_serial_batch_fields": True,
+						"serial_and_batch_bundle": None,
+						"subcontractor": self.subcontractor,
+						"to_main_slip": None,
+						"main_slip": self.main_slip,
+						"qty": mop_batch_list.get(batch) or 0,
+						"manufacturing_operation": row.manufacturing_operation,
+						"custom_manufacturing_work_order": row.manufacturing_work_order,
+						"department": self.department,
+						"to_department": self.department,
+						"manufacturer": self.manufacturer,
+						"material_request": None,
+						"material_request_item": None,
+						"batch_no": batch,
+						"inventory_type": "Regular Stock",
+					},
+				)
+		if temp_diff > 0:
 			frappe.throw(_("Qty not available"))
 
 	se_doc.set_posting_date = 1
