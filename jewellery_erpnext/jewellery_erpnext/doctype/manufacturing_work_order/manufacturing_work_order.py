01124659131636d812a115160b9cb0cabe19321a
Gurukrupa-Export <129185339+Gurukrupa-Export@users.noreply.github.com>
2025-08-29 17:24:49 +0530

    Update manufacturing_work_order.py
    Finished Status of Last operation after Submitting last FG MWO

--- jewellery_erpnext/jewellery_erpnext/doctype/manufacturing_work_order/manufacturing_work_order.py
+++ jewellery_erpnext/jewellery_erpnext/doctype/manufacturing_work_order/manufacturing_work_order.py
@@ -39,6 +39,11 @@ class ManufacturingWorkOrder(Document):
 	def on_submit(self):
 		if self.for_fg:
 			self.validate_other_work_orders()
+			last_department = frappe.db.get_value("Department Operation", {"is_last_operation": 1, "manufacturer": self.manufacturer}, "department")
+			mop_list = frappe.db.get_list("Manufacturing Operation",filters={"department": last_department},pluck="name")
+			if mop_list:
+				for mop in mop_list:
+					frappe.db.set_value("Manufacturing Operation",mop,"status":"Finished")
 		create_manufacturing_operation(self)
 		if self.split_from:
 			create_mr_for_split_work_order(self.name,self.company,self.manufacturer)
