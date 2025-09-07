e5b2538f1bd286c2a5418e8d15f4061f2f9168c6
Kavin <78342682+kavin0411@users.noreply.github.com>
2025-09-08 00:30:22 +0530

    fix: gross wt calculation fix

--- jewellery_erpnext/jewellery_erpnext/doctype/manufacturing_operation/manufacturing_operation.py
+++ jewellery_erpnext/jewellery_erpnext/doctype/manufacturing_operation/manufacturing_operation.py
@@ -1552,13 +1552,13 @@ def get_material_wt(doc):
 				gemstone_pcs += int(str_pcs)
 			else:
 				other_wt += row.qty
+	gross_wt = (net_wt + finding_wt + diamond_wt_in_gram + gemstone_wt_in_gram + other_wt)
 	if  doc.main_slip_no or doc.is_finding:
-		if not frappe.db.get_value("Manufacturing Operation", doc.name,"is_received_gross_greater_than"):
-			gross_wt = net_wt + finding_wt + diamond_wt_in_gram + gemstone_wt_in_gram + other_wt+ abs(doc.loss_wt or 0)
+		if not frappe.db.get_value("Manufacturing Operation", doc.name,"is_received_gross_greater_than") and  doc.is_finding:
+			gross_wt = (net_wt + finding_wt + diamond_wt_in_gram + gemstone_wt_in_gram + other_wt)+ abs(doc.loss_wt or 0)
 		else:
-			gross_wt = (net_wt + finding_wt + diamond_wt_in_gram + gemstone_wt_in_gram + other_wt) - abs(doc.loss_wt or 0)
-	else:
-		gross_wt = net_wt + finding_wt + diamond_wt_in_gram + gemstone_wt_in_gram + other_wt
+			if doc.is_finding:
+				gross_wt = (net_wt + finding_wt + diamond_wt_in_gram + gemstone_wt_in_gram + other_wt) - abs(doc.loss_wt or 0)
 	result = {
 		"gross_wt": gross_wt,
 		"net_wt": net_wt,
