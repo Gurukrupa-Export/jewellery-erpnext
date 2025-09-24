1a6c1528d811dae8655d4743868e19cc21c31e67
Dharm Vaghasiya <160316964+dharm-gkexport@users.noreply.github.com>
2025-09-24 16:20:38 +0530

    Update stock_entry.py
    Fetch manufacturer from Source main slip in stock Entry.

--- jewellery_erpnext/jewellery_erpnext/doc_events/stock_entry.py
+++ jewellery_erpnext/jewellery_erpnext/doc_events/stock_entry.py
@@ -55,11 +55,17 @@ def before_validate(self, method):
 				# pure_item = frappe.db.get_value("Manufacturing Setting", self.company, "pure_gold_item")
 
 				if self.stock_entry_type == 'Material Transfer (MAIN SLIP)':
-					manufacturer = frappe.db.get_value("Main Slip",self.to_main_slip,"manufacturer")
+					if self.to_main_slip:
+						manufacturer = frappe.db.get_value("Main Slip",self.to_main_slip,"manufacturer")
+					if self.main_slip:
+						manufacturer = frappe.db.get_value("Main Slip",self.main_slip,"manufacturer")
 				elif self.manufacturing_order:
 					manufacturer = frappe.db.get_value("Parent Manufacturing Order",self.manufacturing_order,"manufacturer")
 				else:
-					manufacturer = frappe.defaults.get_user_default("manufacturer")
+					if self.manufacturer:
+						manufacturer = self.manufacturer
+					else:
+						manufacturer = frappe.defaults.get_user_default("manufacturer")
 
 				pure_item = frappe.db.get_value("Manufacturing Setting", {"manufacturer":manufacturer}, "pure_gold_item")
 
