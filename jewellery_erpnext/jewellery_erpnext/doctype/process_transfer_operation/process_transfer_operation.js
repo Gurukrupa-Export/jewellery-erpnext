c8ae06f98a80ba207580561699ba85b29709d0be
Dharm Vaghasiya <dharm_v@gkexport.com>
2025-10-02 10:00:25 +0530

    Add Process Transfer Doctype in EXT
    Add Process Transfer Doctype in EXT

---
+++ jewellery_erpnext/jewellery_erpnext/doctype/process_transfer_operation/process_transfer_operation.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Nirali and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Process Transfer Operation", {
+// 	refresh(frm) {
+
+// 	},
+// });
