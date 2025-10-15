8548a4b8b859cb95efcdbe9c635878a301de0292
Dhinesh <79265267+DHINESH00@users.noreply.github.com>
2025-10-15 16:32:22 +0530

    fix: MAX_WRITES_PER_TRANSACTION

--- jewellery_erpnext/jewellery_erpnext/customization/submission_queue/submission_queue.py
+++ jewellery_erpnext/jewellery_erpnext/customization/submission_queue/submission_queue.py
@@ -8,7 +8,7 @@ class CustomSubmissionQueue(SubmissionQueue):
 	def insert(self, to_be_queued_doc: Document, action: str):
 		queue =frappe.db.get_value("Submission Queue",{"ref_doctype":self.ref_doctype,"ref_docname":self.ref_docname,"status":["in",["Queued","Finished"]]})
 
-		if self.ref_doctype in ["Employee IR","Department IR","Product Certification","Stock Entry"] and queue:
+		if self.ref_doctype in ["Employee IR","Department IR","Product Certification","Stock Entry","Main Slip"] and queue:
 			frappe.msgprint(
 			_("Queued for Submission. You can track the progress over {0}.").format(
 				f"<a href='/app/submission-queue/{queue}'><b>here</b></a>"
@@ -20,7 +20,7 @@ class CustomSubmissionQueue(SubmissionQueue):
 			super().insert(to_be_queued_doc,action)
 
 	def after_insert(self):
-		if self.ref_doctype in ["Employee IR","Department IR","Product Certification","Stock Entry"]:
+		if self.ref_doctype in ["Employee IR","Department IR","Product Certification","Stock Entry","Main Slip"]:
 			self.queue_action(
 				"background_submission",
 				to_be_queued_doc=self.queued_doc,
