90d5eaee69a37b8704ce5d2ec391a385fb2a00cb
Kavin <78342682+kavin-114@users.noreply.github.com>
2025-10-24 10:31:29 +0530

    fix: change data small text to data only for mop

--- jewellery_erpnext/patches/remove_se_custom_fields.py
+++ jewellery_erpnext/patches/remove_se_custom_fields.py
@@ -1,13 +1,10 @@
 import frappe
 
 def execute():
-    for fieldname in [
-        "custom_manufacturing_work_order",
-        "custom_parent_manufacturing_order",
-        "manufacturing_operation",
-    ]:
-        if docname:= frappe.db.exists("Custom Field", {"dt": "Stock Entry Detail", "fieldtype":"Small Text", "fieldname": fieldname}):
-            frappe.delete_doc(
-                "Custom Field", docname, force=True
-            )
-            print(f"Deleted Custom Field: {fieldname}")
+    fieldname = "manufacturing_operation"
+
+    if docname:= frappe.db.exists("Custom Field", {"dt": "Stock Entry Detail", "fieldtype":"Small Text", "fieldname": fieldname}):
+        frappe.delete_doc(
+            "Custom Field", docname, force=True
+        )
+        print(f"Deleted Custom Field: {fieldname}")
