adabdc63bb9ac35b752526706cd17c54b3879217
Shruti Bansal <shruti_b@gkexport.com>
2026-02-12 17:42:12 +0530

    Update Making Charge Finding Json
    Update Making Charge Finding Json

--- jewellery_erpnext/jewellery_erpnext/doctype/manufacturing_operation/manufacturing_operation.py
+++ jewellery_erpnext/jewellery_erpnext/doctype/manufacturing_operation/manufacturing_operation.py
@@ -2301,7 +2301,7 @@ def create_finished_goods_bom(self, se_name, mo_data, total_time=0):
 					making_charge_price_subcategories = frappe.get_all(
 						"Making Charge Price Finding Subcategory",
 						filters={"parent": making_charge_price_list[0]["name"]},
-						fields=["subcategory", "rate_per_gm", "supplier_fg_purchase_rate", "wastage", "custom_subcontracting_rate", "custom_subcontracting_wastage"]
+						fields=["subcategory", "rate_per_gm", "supplier_fg_purchase_rate", "wastage", "subcontracting_rate", "subcontracting_wastage"]
 					)
 					# frappe.throw(f"{making_charge_price_subcategories}")
 					if making_charge_price_subcategories:
