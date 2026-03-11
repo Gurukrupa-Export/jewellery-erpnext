900165044d812d77cf46e6542d5e30138fa6b477
DHINESH00 <dhinesh20001406@gmail.com>
2026-03-11 10:42:33 +0530

    fix: add voucher_qty with based on addition_maximum_item__tolerance_percentage

--- jewellery_erpnext/jewellery_erpnext/doctype/mop_settings/mop_settings.js
+++ jewellery_erpnext/jewellery_erpnext/doctype/mop_settings/mop_settings.js
@@ -3,6 +3,25 @@
 
 // frappe.ui.form.on("MOP Settings", {
 // 	refresh(frm) {
-
+// 		// Bind the Sync MOP Log button
+// 		frm.fields_dict.sync_mop_log.$input.on("click", function () {
+// 			frappe.call({
+// 				method: "sync_mop_log",
+// 				doc: frm.doc,
+// 				freeze: true,
+// 				freeze_message: __("Processing EOD MOP Log Sync..."),
+// 				callback: function (r) {
+// 					if (r.message) {
+// 						frappe.show_alert({
+// 							message: __(
+// 								"{0} logs processed, {1} Stock Entries created",
+// 								[r.message.processed, r.message.stock_entries.length]
+// 							),
+// 							indicator: "green",
+// 						});
+// 					}
+// 				},
+// 			});
+// 		});
 // 	},
 // });
