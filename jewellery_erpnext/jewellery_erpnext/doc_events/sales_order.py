aab798f975882502690497e23aec10dc66c53f55
Shruti Bansal <shruti_b@gkexport.com>
2026-04-14 22:12:26 +0530

    Update Sales Order .py
    Update Sales Order .py

--- jewellery_erpnext/jewellery_erpnext/doc_events/sales_order.py
+++ jewellery_erpnext/jewellery_erpnext/doc_events/sales_order.py
@@ -1828,7 +1828,8 @@ def create_new_bom1(self):
 				
 				self.total=self.total + row.amount
 				# ///////////////////////////////////////////////////////////////////////
-				doc.save(ignore_permissions=True)		
+				doc.save(ignore_permissions=True,ignore_validate = True)
+						
 				frappe.db.commit()
 				# frappe.throw(f"{self.total}")
 				
