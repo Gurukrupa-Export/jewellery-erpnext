2858387392abe13f962cd3d38dfe258f85fe2f24
Shruti Bansal <shruti_b@gkexport.com>
2026-05-29 15:13:28 +0530

    Update Sales Order
    Updated Fields Name

--- jewellery_erpnext/jewellery_erpnext/doc_events/sales_order.py
+++ jewellery_erpnext/jewellery_erpnext/doc_events/sales_order.py
@@ -3093,8 +3093,8 @@ def _process_single_row(self, row, ctx):
         # row.bom = row.custom_tracking_bom
         frappe.db.set_value("Tracking Bom", row.custom_tracking_bom, {
             "bom_type":                "Sales Order",
-            "custom_creation_doctype": "Sales Order",
-            "custom_creation_docname": self.name,
+            "reference_doctype": "Sales Order",
+            "reference_docname": self.name,
             "gold_rate_with_gst":      self.gold_rate_with_gst,
         })
         doc = frappe.get_doc("Tracking Bom", row.custom_tracking_bom)
