d06a8417bba527f98bdbfea4db4504466cec4952
Shruti Bansal <shruti_b@gkexport.com>
2026-07-31 12:26:32 +0530

    Added Patch
    Added Patch

--- jewellery_erpnext/patches/customize_fields.py
+++ jewellery_erpnext/patches/customize_fields.py
@@ -7,9 +7,7 @@ def execute():
 	path = os.path.join(os.path.dirname(__file__), "../jewellery_erpnext/custom_fields")
 	for file in os.listdir(path):
 		if file in [
-			"stock_entry_mop_item.json",
-			"stock_entry.json",
-			"stock_entry_detail.json"
+			"sales_order.json","sales_order_item.json","delivery_note.json","delivery_note_item.json","sales_invoice.json","sales_invoice_item.json"
 		]:
 			with open(os.path.join(path, file), "r") as f:
 				CUSTOM_FIELDS.update(json.load(f))
