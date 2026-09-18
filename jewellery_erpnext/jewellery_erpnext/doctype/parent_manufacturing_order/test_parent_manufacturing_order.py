e305445a87ac393f9cbd8095ba0b69536e6f7e99
Kanishkar V <163565326+Kanishkar16Vijay@users.noreply.github.com>
2026-09-18 21:48:52 +0530

    Revert "Revert "feat: rename close to nova glow and update related references""

--- jewellery_erpnext/jewellery_erpnext/doctype/parent_manufacturing_order/test_parent_manufacturing_order.py
+++ jewellery_erpnext/jewellery_erpnext/doctype/parent_manufacturing_order/test_parent_manufacturing_order.py
@@ -500,7 +500,7 @@ def create_man_plan(self):
 	man_plan.company = "Test_Company"
 	man_plan.branch = self.branch
 	if man_plan.setting_type:
-		man_plan.setting_type = "Close"
+		man_plan.setting_type = "Nova Glow"
 	man_plan.is_subcontracting = "No"
 	man_plan.save()
 	man_plan.submit()
